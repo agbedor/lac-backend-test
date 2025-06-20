740084d4609d8b81b41b5d90f6d7d6ea5d52e8b7
agbedeezy <derrickagbedor314@gmail.com>
2025-06-20 12:07:34 +0000

    railway

--- lac/settings.py
+++ lac/settings.py
@@ -97,7 +97,7 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         # "LOCATION": "redis://127.0.0.1:6379/1",  # Use DB 1
-        "LOCATION": os.environ["REDIS_URL"],
+        "LOCATION": os.getenv["REDIS_URL"],
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
