c057ff0a813eb55df659c068a9f188912fefc639
agbedeezy <derrickagbedor314@gmail.com>
2025-06-20 13:49:14 +0000

    update env

--- lac/settings.py
+++ lac/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+CORS_ALLOW_CREDENTIALS = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]  # clean spaces and remove empties
 
