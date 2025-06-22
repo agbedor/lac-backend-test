387d62f83daaed14329ab84be79e7ff275fe978a
agbedeezy <derrickagbedor314@gmail.com>
2025-06-22 11:29:34 +0000

    update env

--- lac/settings.py
+++ lac/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 print("==== DEBUG: SECRET_KEY is:", os.getenv('SECRET_KEY'))
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
