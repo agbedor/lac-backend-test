cf9998474fc116ea4d28950015801a964556190d
agbedeezy <derrickagbedor314@gmail.com>
2025-06-22 11:59:33 +0000

    update env

--- lac/settings.py
+++ lac/settings.py
@@ -12,11 +12,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")  #
 print("==== DEBUG: SECRET_KEY is:", os.getenv('SECRET_KEY'))
 print("==== DEBUG: TEST_VAR is:", os.getenv('TEST_VAR'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
